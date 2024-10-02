3e3365d9cd3a4c6fc038135123a2114aa90f2f3a
JeZaKa <zaicka.zhen@ukr.net>
2024-10-03 01:27:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import sun from './assets/sun.png'
+import cloud from './assets/cloud.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='wrapper'>
+        <h1>Доброе Утро</h1>
+        <img className='cloud cloud1' src={cloud} alt="cloud" />
+        <img className='cloud cloud2' src={cloud} alt="cloud" />
+        {/* <img className='cloud cloud3' src={cloud} alt="cloud" /> */}
+        <img className='cloud cloud4' src={cloud} alt="cloud" />
+        <img className='logoSun' src={sun} alt="sun" />
+        <h2>Солнышко</h2>
+      </div>
     </div>
   );
 }
